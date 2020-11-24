01da586bc096b4e487e1ad4ef6b5aa10ba89625e
CarlosJair117 <carlos.fdez.guar19@gmail.com>
2020-11-23 19:41:12 -0600

    commit 7 conectando BD

---
+++ app/src/main/java/com/jair/conf/model/Ubication.kt
@@ -0,0 +1,11 @@
+package com.jair.conf.model
+
+class Ubication {
+    val name = ""
+    val address = ""
+    val latitude = ""
+    val longitude = ""
+    val phone = ""
+    val webside = ""
+    val photo = ""
+}
