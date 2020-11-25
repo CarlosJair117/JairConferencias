9d1678a6d60e2d07efefe1213d43baeed440fa73
CarlosJair117 <carlos.fdez.guar19@gmail.com>
2020-11-25 01:31:05 -0600

    commit 10 most detalle Schedule

--- app/src/main/java/com/jair/conf/model/Speaker.kt
+++ app/src/main/java/com/jair/conf/model/Speaker.kt
@@ -1,6 +1,8 @@
 package com.jair.conf.model
 
-class Speaker {
+import java.io.Serializable
+
+class Speaker: Serializable {
     var name = ""
     var jobtitle = ""
     var workplace = ""
--- app/src/main/java/com/jair/conf/view/ui/fragments/ScheduleDetailDialogFragment.kt
+++ app/src/main/java/com/jair/conf/view/ui/fragments/ScheduleDetailDialogFragment.kt
@@ -1,11 +1,18 @@
 package com.jair.conf.view.ui.fragments
 
+import android.graphics.Color
 import android.os.Bundle
+import android.telecom.Conference
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
 import com.jair.conf.R
+import com.jair.conf.model.conference
+import kotlinx.android.synthetic.main.fragment_schedule_detail_dialog.*
+import java.text.SimpleDateFormat
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,7 +24,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ScheduleDetailDialogFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ScheduleDetailDialogFragment : Fragment() {
+class ScheduleDetailDialogFragment : DialogFragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -28,6 +35,7 @@ class ScheduleDetailDialogFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialogStyle)
     }
 
     override fun onCreateView(
@@ -36,6 +44,33 @@ class ScheduleDetailDialogFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_schedule_detail_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        toolbarConference.navigationIcon = ContextCompat.getDrawable(view.context, R.drawable.ic_close)
+        toolbarConference.setTitleTextColor(Color.WHITE)
+        toolbarConference.setNavigationOnClickListener{
+            dismiss()
+        }
+
+        val Conference = arguments?.getSerializable("conference") as conference
+        toolbarConference.title = Conference.title
+
+        tvItemScheduleTituloConferencia.text = Conference.title
+        val pattern = "dd/MM/yyyy hh:mm a"
+        val simpleDF = SimpleDateFormat (pattern)
+        val date = simpleDF.format(Conference.datetime)
+        tvDetailConferenceHour.text = date
+        tvDetailConferenceSpeaker.text = Conference.speaker
+        tvDetailConferenceTag.text = Conference.tag
+        tvDetailConferenceDescription.text = Conference.description
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
     }
 
     companion object {
